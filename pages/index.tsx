524d8308246f05ee72a8b37267b3c64012d90be9
Kain <rx1914@hotmail.com>
2023-12-09 02:20:47 -0600

    demo app

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,15 @@
 import { useState, useEffect } from 'react';
 import { generateClient } from 'aws-amplify/data';
 import { Schema } from '@/amplify/data/resource';
+import { Button, Card, Flex, Grid, Heading, Input, Label } from '@aws-amplify/ui-react';
+
 
 // generate your data client using the Schema from your backend
 const client = generateClient<Schema>();
 
 export default function HomePage() {
   const [todos, setTodos] = useState<Schema['Todo'][]>([]);
+  const [order, setOrder] = useState<string>();
 
   async function listTodos() {
     // fetch all todos
@@ -22,42 +25,64 @@ export default function HomePage() {
   
   }, []);
 
+  const handleSubmit = async () => {
+
+    // create a new Todo with the following attributes
+    const { errors, data: newTodo } = await client.models.Todo.create({
+
+      // prompt the user to enter the title
+
+      content: order,
+
+      done: false,
+
+      priority: 'medium'
+
+    })
+
+    console.log(errors, newTodo);
+
+  };
+
   return (
-    <main>
-
-      <h1>Hello, Amplify 👋</h1>
-
-      <button onClick={async () => {
-
-        // create a new Todo with the following attributes
-
-        const { errors, data: newTodo } = await client.models.Todo.create({
-
-          // prompt the user to enter the title
-
-          content: window.prompt("title"),
-
-          done: false,
-
-          priority: 'medium'
-
-        })
-
-        console.log(errors, newTodo);
-
-      }}>Create </button>
-
-
-      <ul>
-
+  <Grid
+    columnGap="0.5rem"
+    rowGap="0.5rem"
+    templateColumns="1fr 1fr 1fr"
+    templateRows="1fr 9fr 1fr"
+    color="black"
+  >
+    <Card
+      columnStart="1"
+      columnEnd="-1"
+    >
+      <Heading level={1} textAlign={'center'}>Sophie Burguer 🍔</Heading>
+    </Card>
+    <Card
+      columnStart="1"
+      columnEnd="2"
+    >
+      <Flex direction="column" gap="small">
+        <Heading level={2} textAlign={'center'}> Menú</Heading>
+        <Input id="order" type="text" isRequired onChange={e => setOrder(e.currentTarget.value)}/>
+        <Button type="button" onClick={handleSubmit}>Enviar pedido</Button>
+      </Flex>
+      
+    </Card>
+    <Card
+      columnStart="2"
+      columnEnd="-1"
+    >
+      <Heading level={2} textAlign={'center'}> Pedidos activos</Heading>
+    
+      <Flex direction={'column'} maxWidth={'400px'}>
         {todos.map((todo) => (
-
-          <li key={todo.id}>{todo.content}</li>
-
+          <Card variation="outlined" key={todo.id}>{todo.content}</Card>
         ))}
-
-      </ul>
-
-    </main>
+      </Flex>
+      
+    </Card>
+  </Grid>
+    
   );
 }
